5a322ad97c81137860c71886d0e1ec085dea346a
NFartenadze <nfartenadze@solvd.com>
2023-12-01 15:06:58 +0400

    refactor: remove unnecessary code

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
-// import { main } from "./main";
 import * as dotenv from "dotenv";
-import { User as UserClass } from "./classes/user";
 import { getUsers, updateUser } from "./services/userService";
-
-// const app = express();
-// const port = 3000;
-
-// app.use(express.json());
-
-// app.get("/", (req, res) => {
-//   res.send("Hello");
-// });
-
-// get users from database
-// app.get("/users", async (req, res) => {
-//   try {
-//     const users = await User.find({});
-//     res.status(200).json(users);
-//   } catch (error: any) {
-//     console.log(error);
-//     res.status(500).json({ message: error.message });
-//   }
-// });
-
-// //insert user to database
-// app.post("/users", async (req, res) => {
-//   try {
-//     const user = await User.create(req.body);
-//     res.status(200).json(user);
-//   } catch (error: any) {
-//     console.log(error);
-//     res.status(500).json({ message: error.message });
-//   }
-// });
-
-// main();
 
 dotenv.config();
 
@@ -44,13 +9,7 @@ mongoose
   .connect(process.env.MONGODB_URI!)
   .then(() => {
     console.log("Connected to MongoDb");
-    // app.listen(port, () => {
-    //   console.log(`Server is running at http://localhost:${port}`);
-    // });
   })
   .catch((error) => {
     console.log(error);
   });
-
-getUsers();
-updateUser("65696494d30ffcdaafed1353", "john");
